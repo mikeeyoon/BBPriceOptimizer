7ed145fe4b8a03910528154f589565725d07daec
Lyndon Won <lwon@shaw.ca>
2017-04-01 15:57:45 -0700

    got rid of old team name

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
 
 router.get('/search', function(req, res, next) {
