1ec99b03e025415ca23d9c8e4402615271cba24b
mikeeyoon <mikeyoon@hotmail.ca>
2017-04-01 03:18:25 -0700

    made few changes to pricing engine

--- pricing-engine.js
+++ pricing-engine.js
@@ -5,10 +5,19 @@ var p2;
 var p3;
 var p4;
 
-if (az > bb || az < bb) {
-  diff = bb - az;
-  p1 = az + (diff*0.25);
-  p2 = az + (diff*0.5);
-  p3 = az + (diff*0.75);
-  p4 = az;
+var lowest_price;
+var high_price;
+
+if(az < bb){
+    lowest_price = az;
+    high_price = bb;
+}else{
+    lowest_price = bb;
+    high_price = az;
 }
+
+var diff = high_price - lowest_price;
+p1 = lowest_price + (diff*0.25);
+p2 = lowest_price + (diff*0.5);
+p3 = lowest_price + (diff*0.75);
+p4 = high_price;
