f876a12b642e9370af4f187fe1620983fcadeae5
mikeeyoon <mikeyoon@hotmail.ca>
2017-04-01 03:21:29 -0700

    made pricing engine a function instead

--- pricing-engine.js
+++ pricing-engine.js
@@ -8,16 +8,19 @@ var p4;
 var lowest_price;
 var high_price;
 
-if(az < bb){
-    lowest_price = az;
-    high_price = bb;
-}else{
-    lowest_price = bb;
-    high_price = az;
-}
+function calculatePrices(az, bb){
 
-var diff = high_price - lowest_price;
-p1 = lowest_price + (diff*0.25);
-p2 = lowest_price + (diff*0.5);
-p3 = lowest_price + (diff*0.75);
-p4 = high_price;
+    if(az < bb){
+        lowest_price = az;
+        high_price = bb;
+    }else{
+        lowest_price = bb;
+        high_price = az;
+    }
+
+    var diff = high_price - lowest_price;
+    p1 = lowest_price + (diff*0.25);
+    p2 = lowest_price + (diff*0.5);
+    p3 = lowest_price + (diff*0.75);
+    p4 = high_price;
+}
